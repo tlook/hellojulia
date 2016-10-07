3d4de2bd890e36c54c8a57fca29453797377c9e3
Mac Lee <macthecadillac@gmail.com>
2016-10-06 22:14:41 -0700

    Partial rewrote
    Moved a ton of functions to qumath.jl.
    Also removed DenseBra, DenseKet, SparseBra, SparseKet for Bra and Ket types.

--- qutypes.jl
+++ qutypes.jl
@@ -1,21 +1,14 @@
-module QuTypes
-
 # TODO: Make returns of slicing conform to my type
 # TODO: Create promotion functions/definitions
 
 export QuantumType, AbstractState, AbstractOperator,
-    Bra, Ket,
-    DenseOp, DenseBra, DenseKet,
-    SparseOp, SparseBra, SparseKet,
+    Bra, Ket, DenseOp, SparseOp,
     rawdata
 
 # Abstract type definitions
 abstract QuantumType
 abstract AbstractState <: QuantumType
 abstract AbstractOperator <: QuantumType
-
-abstract Bra <: AbstractState
-abstract Ket <: AbstractState
 
 # Concrete type definitions
 type DenseOp{T<:Number} <: AbstractOperator
@@ -28,41 +21,21 @@ type SparseOp{T<:Number} <: AbstractOperator
     SparseOp(A) = new(A)
 end
 
-type DenseBra{T<:Number} <: Bra
+type Bra{T<:Number} <: AbstractState
     dat::Array{T, 2}
-    function DenseBra(ψ)
+    function Bra(ψ)
         if length(size(ψ)) ≠ 2 || size(ψ)[1] ≠ 1
-            error("A DenseBra must be a row vector.")
+            error("A Bra must be a row vector.")
         end
         new(ψ)
     end
 end
 
-type SparseBra{T<:Number} <: Bra
-    dat::SparseMatrixCSC{T, Int64}
-    function SparseBra(ψ)
-        if length(size(ψ)) ≠ 2 || size(ψ)[1] ≠ 1
-            error("A SparseBra must be a row vector.")
-        end
-        new(ψ)
-    end
-end
-
-type DenseKet{T<:Number} <: Ket
+type Ket{T<:Number} <: AbstractState
     dat::Array{T, 1}
-    function DenseKet(ψ)
+    function Ket(ψ)
         if length(size(ψ)) ≠ 1
             error("A DenseKet must be a column vector.")
-        end
-        new(ψ)
-    end
-end
-
-type SparseKet{T<:Number} <: Ket
-    dat::SparseMatrixCSC{T, Int64}
-    function SparseKet(ψ)
-        if length(size(ψ)) ≠ 1
-            error("A SparseKet must be a column vector.")
         end
         new(ψ)
     end
@@ -71,33 +44,21 @@ end
 # Constructor functions
 SparseOp{T<:Number}(mat::SparseMatrixCSC{T, Int64}) = SparseOp{T}(mat)
 DenseOp{T<:Number}(mat::Array{T, 2}) = DenseOp{T}(mat)
-SparseBra{T<:Number}(vec::SparseMatrixCSC{T, Int64}) = SparseBra{T}(vec)
-DenseBra{T<:Number}(vec::Array{T, 2}) = DenseBra{T}(vec)
-SparseKet{T<:Number}(vec::SparseVector{T, Int64}) = SparseKet{T}(vec)
-DenseKet{T<:Number}(vec::Array{T, 1}) = DenseKet{T}(vec)
+Bra{T<:Number}(vec::Array{T, 2}) = Bra{T}(vec)
+Ket{T<:Number}(vec::Array{T, 1}) = Ket{T}(vec)
 
 # Functions regarding sparsity
-Base.sparse(a::DenseBra) = SparseBra(sparse(a.dat))
-Base.sparse(a::DenseKet) = SparseKet(sparse(a.dat))
 Base.sparse(a::DenseOp) = SparseOp(sparse(a.dat))
-Base.full(a::SparseBra) = DenseBra(full(a.dat))
-Base.full(a::SparseKet) = DenseKet(full(a.dat))
 Base.full(a::SparseOp) = DenseOp(full(a.dat))
 SparseOp(a::DenseOp) = sparse(a)
-SparseBra(a::DenseBra) = sparse(a)
-SparseKet(a::DenseKet) = sparse(a)
 DenseOp(a::SparseOp) = full(a)
-DenseBra(a::SparseBra) = full(a)
-DenseKet(a::SparseKet) = full(a)
 
 # Convert Bra to Ket and vice versa
 Base.ctranspose{T<:AbstractOperator}(a::T) = T(ctranspose(a.dat))
-Base.ctranspose(a::DenseBra) = DenseKet(ctranspose(a.dat))
-Base.ctranspose(a::SparseBra) = SparseKet(ctranspose(a.dat))
-Base.ctranspose(a::DenseKet) = DenseBra(ctranspose(a.dat))
-Base.ctranspose(a::SparseKet) = SparseBra(ctranspose(a.dat))
+Base.ctranspose(a::Bra) = Ket(ctranspose(a.dat)[:, 1])
+Base.ctranspose(a::Ket) = Bra(ctranspose(a.dat))
 
-# Other redefinitions
+# Other redefinitions:bn
 Base.length{T<:QuantumType}(a::T) = length(a.dat)
 Base.size{T<:QuantumType}(a::T) = size(a.dat)
 Base.ndims{T<:QuantumType}(a::T) = ndims(a.dat)
@@ -114,12 +75,14 @@ Base.copy{T<:QuantumType}(a::T) = T(copy(a.dat))
 Base.summary{T<:AbstractOperator}(a::T) = "$(size(a.dat)[1])x$(size(a.dat)[2]) $T"
 Base.summary{T<:Bra}(a::T) = "Length $(size(a.dat)[2]) $T"
 Base.summary{T<:Ket}(a::T) = "Length $(size(a.dat)[1]) $T"
+
 function Base.show(io::IO, X::QuantumType)
-    println(io, summary(X))
+    print(io, summary(X))
+    println(io, ":")
     print(io, X.dat)
 end
+
 rawdata{T<:QuantumType}(a::T) = a.dat
 
 # Promotion rules
-
-end
+#convert{T<:Number}(::
